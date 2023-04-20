ff17a0965bf9d5a895a77c379ef3fbe43fa915cb
RenanBelo <71426661+RenanBelo@users.noreply.github.com>
2023-04-20 09:07:09 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,53 +1,18 @@
-import Head from 'next/head'
-import { Inter } from 'next/font/google'
-import { useId } from 'react';
-
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
-
-  function handleSubmit(e) {
-    //Evita o recarregamento da página quando o form é enviado.
-    e.preventDefault();
-
-    //Lê e manipula os dados do form.
-    const form = e.target;
-    const formData = new FormData(form);
-
-    //Envia os dados para uma API
-    fetch('/endereco-da-API', { method: form.method, body: formData });
-
-    //Transforma os dados do form em JSON
-    const formJson = Object.fromEntries(formData.entries());
-    console.log(formJson);
-  }
-
-  const pesquisaTextAreaId = useId();
-
-  return (
-    <>
-      <Head>
-        <title>Procuritiba</title>
-        <meta name="description" content="Encontre o rolê ideal para você!" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      
-      <form method="post" onSubmit={handleSubmit}>
-        <div className="form-items-container">
-          <label htmlFor={pesquisaTextAreaId}>
-            Quais os requisitos para o rolê de hoje?
-          </label>
-          <textarea
-            id={pesquisaTextAreaId}
-            name="userRoleDetails"
-            rows={4}
-            cols={40}
-          />      
-          
-          <button type="submit">pesquisar</button>
-        </div>
-      </form>
-    </>
-  )
-}
+function mostrarRecomendados(recomendados) {
+            var resultado = document.getElementById("resultado");
+            resultado.innerHTML = "";
+            if (recomendados.length === 0) {
+                resultado.innerHTML = "<p>Nenhum restaurante encontrado.</p>";
+                return;
+            }
+            for (var i = 0; i < recomendados.length; i++) {
+                var restaurante = recomendados[i][0];
+                var media_sentimento = recomendados[i][1];
+                var nome = restaurante.name;
+                var endereco = restaurante.address;
+                var pontuacao = restaurante.rating;
+                var comentario = restaurante.reviews[0].content;
+                var div = document.createElement("div");
+                div.innerHTML = "<h3>" + nome + "</h3><p>" + endereco + "</p><p>Média de sentimento: " + media_sentimento + "</p>";
+                resultado.appendChild(div);
+            }
